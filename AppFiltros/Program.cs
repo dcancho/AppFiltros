f39ab8cf2dc8801a13017bc169f495d04c3e26f1
Diego Fabian Cancho Coila <u20201f479@upc.edu.pe>
2023-04-07 11:59:59 -0500

    none

--- AppFiltros/Program.cs
+++ AppFiltros/Program.cs
@@ -9,27 +9,24 @@
             //initialize explicitely the image
             myImage.Pixels = new byte[,]
             {
-                {0,1,1,2,3 },
-                {1,1,3,4,4},
-                {1,1,2,4,2 },
-                {2,3,2,2,2},
-                {1,1,1,1,1 }
+                {0,4,5,5,5 },
+                {4,4,5,7,5 },
+                {4,4,5,5,5 },
+                {0,4,5,5,5 },
+                {4,4,5,7,5 }
             };
             //Create a filter
             Filter filter = new Filter(3, new sbyte[,]
         {
-                {0,1,0},
-                {1,-2,1},
-                {0,1,0}
+                {1,2,1},
+                {2,4,2},
+                {1,2,1}
             });
             //Print the matrix
             myImage.Print();
             //Apply the filter
             myImage.ApplyFilter(filter);
-            Console.WriteLine("\n");
-            Console.WriteLine("\n");
-            Console.WriteLine("\n");
-            Console.WriteLine("\n");
+            Console.WriteLine("\n\n\n");
             myImage.Print();
         }
 
