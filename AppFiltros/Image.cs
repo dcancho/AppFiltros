ce52d9e80d5de60ed45b3c97499bce47ad4445f3
Diego Fabian Cancho Coila <u20201f479@upc.edu.pe>
2023-04-26 16:55:46 -0500

    first

--- AppFiltros/Image.cs
+++ AppFiltros/Image.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppFiltros
+﻿namespace AppFiltros
 {
     /// <summary>
     /// Tipos de valor admisibles para <see cref="Layer"/>.
@@ -107,6 +100,17 @@ namespace AppFiltros
             MinValue = minValue;
         }
 
+        public Image(string path, bool keepColor)
+        {
+            GetImage(path, keepColor);
+            MaxValue= 255;
+            MinValue = 0;
+            if (Layers == null)
+            {
+                throw new Exception("No se pudo cargar la imagen. Posiblemente es un archivo dañado.");
+            }
+        }
+
 
         /// <summary>
         /// x: Rows
@@ -133,6 +137,171 @@ namespace AppFiltros
         {
             set { Layers[i] = value; }
             get { return Layers[i]; }
+        }
+
+        /// <summary>
+        /// Guarda la imagen con el nombre y formato especificado. Formato por defecto es PNG.
+        /// </summary>
+        /// <param name="path"></param>
+        public void WriteImage(string path)
+        {
+            Image<Rgba32> resultImage = new((int)Columns, (int)Rows);
+            for (uint i = 0; i < Rows; i++)
+            {
+                for (uint j = 0; j < Columns; j++)
+                {
+                    resultImage[(int)j, (int)i] = LayerDepth > 1
+                        ? new Rgba32(this[0][i, j], this[1][i, j], this[2][i, j], 255)
+                        : new Rgba32(this[0][i, j], this[0][i, j], this[0][i, j], 255);
+
+                }
+            }
+            resultImage.SaveAsPng(path);
+        }
+
+
+
+        /// <summary>
+        /// Leer los valores de los pixeles de una imagen. Devuelve una matrix Rgba32. Multithread.
+        /// </summary>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        private static Rgba32[,] ReadPixelValues(Image<Rgba32> image)
+        {
+            int remainingPixels = image.Height * image.Width;
+            int processorsCount = Environment.ProcessorCount;
+            int rowsPerThread = image.Height / processorsCount;
+            Rgba32[,] pixels = new Rgba32[image.Height, image.Width];
+            Thread[] threads = new Thread[processorsCount];
+            object lockObject = new();
+            for (int t = 0; t < processorsCount; t++)
+            {
+                int startRow = t * rowsPerThread;
+                int endRow = (t == processorsCount - 1) ? image.Height : startRow + rowsPerThread;
+                threads[t] = new Thread(() =>
+                {
+                    for (int i = startRow; i < endRow; i++)
+                    {
+                        for (int j = 0; j < image.Width; j++)
+                        {
+                            lock (lockObject)
+                            {
+                                pixels[i, j] = image[j, i];
+                            }
+                            //remainingPixels--;
+                            //Console.WriteLine($"Loading: {remainingPixels} remaining");
+                        }
+                    }
+                });
+                threads[t].Start();
+            }
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+            return pixels;
+        }
+
+        private static Layer ToBlackWhite(Layer[] layers)
+        {
+            Layer output = new(ValueType.Grayscale, layers[0].Rows, layers[0].Columns);
+            Thread[] threads = new Thread[Environment.ProcessorCount];
+            uint numThread = (uint)threads.GetLength(0);
+            uint rowsPerThread = (uint)layers[0].Rows / numThread;
+            object lockObject = new();
+            for (int t = 0; t < numThread; t++)
+            {
+                uint startRow = (uint)t * rowsPerThread;
+                uint endRow = (t == numThread - 1) ? (uint)layers[0].Rows : startRow + rowsPerThread;
+                threads[t] = new Thread(() =>
+                {
+                    for (uint i = startRow; i < endRow; i++)
+                    {
+                        for (uint j = 0; j < layers[0].Columns; j++)
+                        {
+                            lock (lockObject)
+                            {
+                                output[(uint)i, (uint)j] = (byte)((layers[0][i, j] + layers[1][i, j] + layers[2][i, j]) / 3);
+                            }
+                        }
+                    }
+                });
+                threads[t].Start();
+            }
+
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+
+            return output;
+        }
+
+        private static Layer ExtractValuesToLayer(Rgba32[,] image, int type)
+        {
+            var layerType = type switch
+            {
+                0 => ValueType.R,
+                1 => ValueType.G,
+                2 => ValueType.B,
+                3 => ValueType.A,
+                _ => ValueType.R,
+            };
+            byte[,] bytes = new byte[image.GetLength(0), image.GetLength(1)];
+            for (int i = 0; i < image.GetLength(0); i++)
+            {
+                for (int j = 0; j < image.GetLength(1); j++)
+                {
+                    switch (layerType)
+                    {
+                        case ValueType.R:
+                            bytes[i, j] = image[i, j].R;
+                            break;
+                        case ValueType.G:
+                            bytes[i, j] = image[i, j].G;
+                            break;
+                        case ValueType.B:
+                            bytes[i, j] = image[i, j].B;
+                            break;
+                        case ValueType.A:
+                            bytes[i, j] = image[i, j].A;
+                            break;
+                        case ValueType.Grayscale:
+                            break;
+                        default:
+                            bytes[i, j] = image[i, j].R;
+                            break;
+                    }
+                }
+            }
+            return new Layer(layerType, bytes);
+        }
+        public void GetImage(string path, bool keepColor)
+        {
+            Layer[] layers;
+            using (Image<Rgba32> image = SixLabors.ImageSharp.Image.Load<Rgba32>(path))
+            {
+                Rgba32[,] pixels = ReadPixelValues(image);
+                Layer[] extractedLayers = new Layer[pixels.Length];
+                for (int i = 0; i < 4; i++)
+                {
+                    extractedLayers[i] = ExtractValuesToLayer(pixels, i);
+                }
+                if (!keepColor)
+                {
+                    layers = new Layer[1];
+                    layers[0] = ToBlackWhite(extractedLayers);
+                }
+                else
+                {
+                    layers = extractedLayers;
+                }
+            }
+            Rows = layers[0].Rows;
+            Columns = layers[0].Columns;
+            LayerDepth = (byte)layers.Length;
+            Layers = layers;
         }
     }
 }
---
+++ AppFiltros/Script.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+
+namespace AppFiltros
+{
+    public class Script
+    {
+        private static (float[,],int) GetMatrix(float[] array)
+        {
+            float[,] matrix;
+            int size = (int)Math.Sqrt(array.GetLength(0));
+            matrix = new float[size,size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = array[i * size + j];
+                }
+            }
+            return (matrix, size);
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Ingrese la ruta del archivo a filtrar: ");
+            string path = ""+Console.ReadLine();
+            if(path == "")
+            {
+                path = "test.png";
+            }
+            Console.WriteLine("¿Desea mantener el color? (S/N)");
+            string color = ""+Console.ReadLine();
+            bool keepColor;
+            _ = (color.ToUpper() == "S") ? keepColor = true : keepColor = false;
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            Image image = new(path, keepColor);
+            Console.WriteLine($"Image: image has {image.LayerDepth} layers");
+            timer.Stop();
+            Console.WriteLine("Ingrese los valores del filtro. Use comas para delimitar: ");
+            string[] filterValues = ("" + Console.ReadLine()).Split(',');
+            float[] floats = new float[filterValues.Length];
+            for (int i = 0; i < filterValues.Length; i++)
+            {
+                floats[i] = Convert.ToSingle(filterValues[i]);
+            }
+            Console.WriteLine("Ingrese el valor del factor. Negativo para ignorarlo");
+            int factor = Convert.ToInt32(Console.ReadLine());
+            Filter filter = new(GetMatrix(floats).Item2, GetMatrix(floats).Item1, Convert.ToBoolean(factor), (float)1.0f/factor);
+            timer.Start();
+            Image image2 = filter.ApplyFilter(image, true, 255, 0);
+            timer.Stop();
+            Console.WriteLine("Ingrese el nombre del archivo a guardar: ");
+            string newpath = "" + Console.ReadLine();
+            timer.Start();
+            image2.WriteImage(newpath);
+            timer.Stop();
+
+            Console.WriteLine($"Time elapsed: {timer.ElapsedMilliseconds}");
+        }
+    }
+}
